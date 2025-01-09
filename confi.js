a7c0073dfa6c9c58cc5b0c5b067813009aedf391
cobr23 <contactprasad32@gmail.com>
2025-01-10 00:05:50 +0530

    Create confi.js

---
+++ confi.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+OWNER: process.env.OWNER === undefined ? `94711262551` : process.env.OWNER,
+}
